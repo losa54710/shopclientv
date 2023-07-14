bacdc3d77c4c8f80a7420e0444bf5e97970e31d5
losa54710 <65610614+losa54710@users.noreply.github.com>
2023-07-14 13:54:17 -0500

    new server host

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import authHeader from './auth-header';
-const API_URL = `https://shopback00.herokuapp.com/`;
+const API_URL = `https://https://shopback01.vercel.app//`;
 //const API_URL = `http://localhost:3030/`;
 
 class UserService {
